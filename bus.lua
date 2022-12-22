3e79338a446e3eb91c96393a8d75e158ce1a3a03
Signo <adriensignorino@gmail.com>
2022-12-22 11:24:04 +0100

    Update bus.lua
    fix

--- bus.lua
+++ bus.lua
@@ -343,7 +343,7 @@ function process_node(profile, node, result, relations)
       local restricted_by_height = false
       if barrier == 'height_restrictor' then
          local maxheight = Measure.get_max_height(node:get_value_by_key("maxheight"), node)
-         restricted_by_height = maxheight and maxheight < profile.vehicle_height
+         restricted_by_height = tonumber( maxheight ) and tonumber( maxheight ) < tonumber( profile.vehicle_height )
       end
 
       --  make an exception for rising bollard barriers
